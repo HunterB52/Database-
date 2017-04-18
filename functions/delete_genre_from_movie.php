393a380d77fc11eddf04faf4127b8bf01a2d6cd7
HunterB52 <ehbo222@g.uky.edu>
2017-04-17 21:41:50 -0400

    Create delete_genre_from_movie.php

---
+++ functions/delete_genre_from_movie.php
@@ -0,0 +1,11 @@
+<?php
+include 'connection.php';
+if(!empty($_POST['genre_select']) && !empty($_POST['movie_id'])){
+  $genre_id = $mysqli->escape_string($_POST['genre_select']);
+  $movie_id = $mysqli->escape_string($_POST['movie_id']);
+  $genre_query = $mysqli->query("SELECT * FROM is_genres WHERE movie_id=$movie_id");
+  if($genre_query->num_rows != 1){
+    $deletion_query = $mysqli->query("DELETE FROM is_genres WHERE genre_id=$genre_id AND movie_id=$movie_id");
+  }
+}
+?>
