921765ea4a3d017abad37bd94e5fc2c8a4e6ec85
HunterB52 <ehbo222@g.uky.edu>
2017-04-17 21:51:59 -0400

    Create session.php

---
+++ functions/session.php
@@ -0,0 +1,23 @@
+<!--
+  Team Databased 2017: Movie-DB
+  Author(s): Parker Householder
+  Name: session.php
+  Description: This is the PHP code used for establishing a session and session
+  variables to keep track of a user while logged in
+-->
+<?php
+  //creates a session for storing data across pages
+  session_start();
+  if($_SESSION['logged_in'] != true){
+    header("location: ../login.php");
+  }
+  else {
+    $first_name = $_SESSION['first_name'];
+    $middle_name = $_SESSION['middle_name'];
+    $last_name = $_SESSION['last_name'];
+    $admin_tag = $_SESSION['admin_tag'];
+    $user_id = $_SESSION['user_id'];
+    $message = $_SESSION['message'];
+    $status = $_SESSION['status'];
+  }
+?>
