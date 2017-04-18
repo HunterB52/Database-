f5000e50df370a0f1b183ee5f2efe54164a5fbf4
HunterB52 <ehbo222@g.uky.edu>
2017-04-17 21:33:16 -0400

    Create add_genre.php

---
+++ functions/add_genre.php
@@ -0,0 +1,16 @@
+<?php
+  include 'connection.php';
+  include 'session.php';
+  if(isset($_POST['genre']) && !empty($_POST['genre'])){
+    $c_genre = $mysqli->escape_string($_POST['genre']);
+    $insertion_query = $mysqli->query("INSERT INTO GENRE(genre) VALUES ('$c_genre')");
+    if($insertion_query){
+      $_SESSION['status'] = 'Success';
+      $_SESSION['message'] = 'Success! The genre "' . $c_genre . '" was added to the database.';
+      header("location: ../pages/main_page.php");
+    }
+    else{
+      die("Error");
+    }
+  }
+?>
