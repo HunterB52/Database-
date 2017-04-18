ee426980551d91aaa567efaa4e593b4193ac61d0
HunterB52 <ehbo222@g.uky.edu>
2017-04-17 21:36:02 -0400

    Create add_role.php

---
+++ functions/add_role.php
@@ -0,0 +1,16 @@
+<?php
+  include 'connection.php';
+  include 'session.php';
+  if(isset($_POST['role']) && !empty($_POST['role'])){
+    $role_name = $mysqli->escape_string($_POST['role']);
+    $insertion_query = $mysqli->query("INSERT INTO ROLE(role) VALUES ('$role_name')");
+    if($insertion_query){
+      $_SESSION['status'] = 'Success';
+      $_SESSION['message'] = 'Success! The role "' . $role_name . '" was added to the database.';
+      header("location: ../pages/crews_page.php");
+    }
+    else{
+      die("Error");
+    }
+  }
+?>
