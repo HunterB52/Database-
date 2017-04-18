7ebc4a24556a47c1c0fb5ef899b3bc093002269f
HunterB52 <ehbo222@g.uky.edu>
2017-04-17 21:32:07 -0400

    Create add_crew_to_movie.php

---
+++ functions/add_crew_to_movie.php
@@ -0,0 +1,9 @@
+<?php
+  include 'connection.php';
+  $crew = $mysqli->escape_string($_POST['crew_select_movie']);
+  $movie_id = $mysqli->escape_string($_POST['movie_select']);
+  $crew_movie_query = $mysqli->query("SELECT * FROM has_crew WHERE crew_id=$crew AND movie_id=$movie_id");
+  if($crew_movie_query->num_rows == 0){
+    $insertion_query = $mysqli->query("INSERT INTO has_crew VALUES ($movie_id, $crew)");
+  }
+?>
