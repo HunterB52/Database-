f4674ad836495faf5cf1c08f2aeaad98385ac390
HunterB52 <ehbo222@g.uky.edu>
2017-04-17 21:31:12 -0400

    Create add_crew.php

---
+++ functions/add_crew.php
@@ -0,0 +1,16 @@
+<?php
+  include 'connection.php';
+  include 'session.php';
+  if(isset($_POST['crew']) && !empty($_POST['crew'])){
+    $crew_name = $mysqli->escape_string($_POST['crew']);
+    $insertion_query = $mysqli->query("INSERT INTO CREW(name) VALUES ('$crew_name')");
+    if($insertion_query){
+      $_SESSION['status'] = 'Success';
+      $_SESSION['message'] = 'Success! The crew "' . $crew_name . '" was added to the database.';
+      header("location: ../pages/crews_page.php");
+    }
+    else{
+      die("Error");
+    }
+  }
+?>
